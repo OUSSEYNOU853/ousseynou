5c3c4f02bce22a143d47aec20a96bc63cb0ef42a
ousseynou diedhiou <diedhiouousseynou53@gmail.com>
2024-03-11 03:46:39 +0000

    partie admin mise à jour, juste quelques fonctionnalités restant

--- education_projet.c
+++ education_projet.c
@@ -32,18 +32,14 @@ typedef struct {
 } DATES;
 
 typedef struct {
-    int code;
-    time_t date_presence;
-} Presence;
-
-typedef struct {
     char expediteur[50];
     char contenu[200];
     enum Etat {envoye = 1, echec, enCours} etat;
+   time_t date_presence;
 } Message;
 
 typedef struct {
-    char nom[50];
+    char cordonnes[100];
     int messagesNonLus;
     Message boiteMessages[20];
 } Etudiant;
@@ -60,8 +56,12 @@ typedef struct {
     enum etat {BLOQUE, ACTIF} etat;
     enum niveau {ADMIN=1, APPRENANT} niveau;
     enum referentielle {DEV_WEB=1, DEV_DATA, REF_DIG} referentielle;
-    Presence presence;
 } Utilisateur;
+typedef struct {
+    int code;
+    time_t date_presence;
+    Utilisateur u;
+} Presence;
 
 DATES saisirDates(void)
 {
@@ -78,9 +78,11 @@ DATES saisirDates(void)
 
     return d;
 }
-void afficheDate(DATES a)
+void afficheDate()
 {
+    DATES a;
     printf("La date et l'heure sont : %02d/%02d/%d %02d:%02d:%02d\n", a.jour, a.mois, a.annee, a.heure, a.minute, a.seconde);
+     saisirDates();
 }
 
 int saisirInt(int min, int max, char message[])
@@ -239,24 +241,75 @@ void gestionEtudiants() {
     printf("---------------------------------------------------------------------------------------\n");
     printf("|----------------------Vous avez choisi la gestion des étudiants.---------------------|\n");
     printf("---------------------------------------------------------------------------------------\n");
-    FILE *fichierUtilisateur = fopen("utilisateurs.txt", "w");
-    if (fichierUtilisateur != NULL) {
-        printf("Liste des étudiants :\n");
-    }
-    fclose(fichierUtilisateur);
-}
-void generationfichierUtilisateurs() {
-    printf("---------------------------------------------------------------------------------------\n");
-    printf("|-----------------Vous avez choisi la génération de fichierUtilisateurs.--------------|\n");
-    printf("---------------------------------------------------------------------------------------\n");
-    FILE *fichierUtilisateur = fopen("utilisateurs.txt", "w");
-    if (fichierUtilisateur != NULL) {
-        fprintf(fichierUtilisateur, "Données générées.\n");
-        fclose(fichierUtilisateur);
-        printf("fichierUtilisateur 'utilisateurs.txt' généré avec succès.\n");
+    printf("\n");
+    FILE *fichier = fopen("utilisateurs.txt", "r");
+    
+    if (fichier != NULL) {
+        Utilisateur u;
+
+        printf("\nDÉTAILS DE TOUS LES ÉTUDIANTS :\n");
+    printf("\n");
+        printf("***********************************************************************************************************\n");
+        printf("\n| %-10s | %-20s | %-21s | %-18s | %-10s | %-10s |\n", "ID", "Nom", "Prénom", "Date Naiss.", "Niveau", "Code");
+        printf("***********************************************************************************************************\n");
+
+        while (fscanf(fichier, "%d %49s %49s %49s %49s %d %d %d %d %d", &u.id, u.email, u.motdepasse, u.nom, u.prenom, &u.date_naissance.jour, &u.date_naissance.mois, &u.date_naissance.annee, &u.niveau, &u.code) == 10) { // Ajustez ce nombre si vous lisez plus de champs
+            if (u.niveau == 2) {
+                printf("| %-10d | %-20s | %-20s | %02d/%02d/%-12d | %-10d | %-10d |\n", u.id, u.nom, u.prenom, u.date_naissance.jour, u.date_naissance.mois, u.date_naissance.annee, u.niveau, u.code);
+                printf("-----------------------------------------------------------------------------------------------------------\n");
+            }
+        }
+
+        fclose(fichier);
     } else {
-        printf("Erreur lors de la génération du fichierUtilisateur.\n");
+        printf("Erreur lors de l'ouverture du fichier.\n");
     }
+
+   printf("\nAppuyez sur 'Q' puis sur Enter pour quitter...:");
+    char quitter;
+    do {
+        scanf(" %c", &quitter); 
+    } while (quitter != 'Q' && quitter != 'q');
+    menuAdmin();
+}
+void generationfichiers() {
+    printf("---------------------------------------------------------------------------------------\n");
+    printf("|-----------------Vous avez choisi la génération de fichiers.--------------|\n");
+    printf("---------------------------------------------------------------------------------------\n");
+    FILE *fichier = fopen("utilisateurs.txt", "r");
+    if (fichier != NULL) {
+        Presence presences[100]; 
+        int nbPresences = 0;
+        printf("\nListe des apprenants présents:\n");
+
+        printf("*");
+        for (int i = 0; i < 77; i++) {
+            printf("*");
+        }
+        printf("*\n");
+
+        printf("| %-9s | %-19s | %-19s  | %-19s |\n", "Code", "Nom", "Prénom", "Date");
+
+        printf("*");
+        for (int i = 0; i < 77; i++) {
+            printf("*");
+        }
+        printf("*\n");
+
+        for (int i = 0; i < nbPresences; i++) {
+          printf("| %-9d | %-19s | %-19s | %-19ld |\n", presences[i].code, presences[i].u.nom, presences[i].u.prenom, presences->date_presence);
+
+            printf("-------------------------------------------------------------------------------\n");
+        }
+    } else {
+        printf("Erreur lors de la génération du fichier.\n");
+    }
+    printf("\nAppuyez sur 'Q' puis sur Enter pour quitter...:");
+    char quitter;
+    do {
+        scanf(" %c", &quitter);
+    } while (quitter != 'Q' && quitter != 'q');
+    menuAdmin();
 }
 void marquerPresences() {
     int quitter = 0;
@@ -269,9 +322,10 @@ void marquerPresences() {
     printf("---------------------------------------------------------------------------------------\n");
 
     do {
-        printf("Saisissez le code de l'étudiant à marquer : ");
+        printf("Saisissez le code de l'étudiant à marquer (Q pour quitter): ");
         scanf("%d", &u.code);
-        trouve = 0;
+            char quitter;
+        FILE *fichier = fopen("utilisateurs.txt", "r");
         FILE *fichierPresence = fopen("presences.txt", "a");
         if (!fichierPresence) {
             printf("Impossible d'ouvrir le fichier des présences.\n");
@@ -293,43 +347,146 @@ void marquerPresences() {
         fprintf(fichierPresence, "id: %d matricule: %s email: %s motdepasse: %s nom: %s prenom: %s Date naissance: %02d/%02d/%04d niveau: %d code : %d\n", u.id, u.matricule, u.email, u.motdepasse, u.nom, u.prenom, u.date_naissance.jour, u.date_naissance.mois, u.date_naissance.annee, u.niveau, u.code);
         DATES saisirDates(void);
         fclose(fichierPresence);
-}while (saisirUtilisateurs(u.code) && !estDejaMarque(presences, nombrePresences));
-        printf("Tapez 1 pour quitter : ");
-        scanf("%d", &quitter);
-        void connexion();
+    }while (saisirUtilisateurs(u.code) && !estDejaMarque(presences, nombrePresences) && !quitter);
+    
 }
+void envoyerMessageATous() {
+    FILE *fichier = fopen("utilisateurs.txt", "r");
+    FILE *messagesFp = fopen("messages.txt", "a");
 
-Etudiant envoyerMessage(char *expediteur, char *contenu) {
-    Etudiant e;
-    if (e.messagesNonLus < 10) {
-        strcpy(e.boiteMessages[e.messagesNonLus].expediteur, expediteur);
-        strcpy(e.boiteMessages[e.messagesNonLus].contenu, contenu);
-        e.messagesNonLus++;
-        printf("Message envoyé à %s : %s\n", e.nom, contenu);
+    if (fichier != NULL && messagesFp != NULL) {
+        Utilisateur u;
+
+        printf("\nENVOYER UN MESSAGE À TOUS LES APPRENANTS:\n");
+
+        printf("Saisissez votre message (appuyez sur Entrée pour terminer):\n");
+
+        char message[500];
+        getchar(); 
+        fgets(message, sizeof(message), stdin);
+
+        fprintf(messagesFp, "Destinataire\tMessage\n");
+
+        while (fscanf(fichier, "%s %s %s %s %s %s %s %s %s %s %s", u.nom, u.prenom, u.email, u.motdepasse) != EOF) {
+            fprintf(messagesFp, "%s %s\t%s\n", u.nom, u.prenom, message);
+        }
+
+        fclose(fichier);
+        fclose(messagesFp);
+        printf("Message envoyé avec succès à tous les apprenants.\n");
+        envoyerDesMessages();
     } else {
-        printf("La boîte de messages de %s est pleine. Impossible d'envoyer le message.\n", e.nom);
+        printf("Erreur lors de l'ouverture des fichiers.\n");
+    } 
+}
+void envoyerMessageParReferentiel() {
+    FILE *fichier = fopen("utilisateurs.txt", "r");
+    FILE *messagesFp = fopen("messages.txt", "a");
+
+    if (fichier != NULL && messagesFp != NULL) {
+        Utilisateur u;
+
+        printf("\nENVOYER UN MESSAGE AUX APPRENANTS D'UN REFERENTIEL:\n");
+
+        char referentiel[20];
+        printf("Saisissez le référentiel (dev web/dev data/ref dig) : ");
+        scanf("%s", referentiel);
+
+        printf("Saisissez votre message (appuyez sur Entrée pour terminer):\n");
+
+        char message[500];
+        getchar();
+        fgets(message, sizeof(message), stdin);
+
+        fprintf(messagesFp, "Destinataire\tMessage\n");
+
+        while (fscanf(fichier, "%s %s %s %s %s %s %s %s %s %s %s", u.nom, u.prenom, u.email, u.motdepasse) != EOF) {
+            if (strcmp(u.referentielle, referentiel) == 0) {
+                fprintf(messagesFp, "%s %s\t%s\n", u.nom, u.prenom, message);
+            }
+        }
+
+        fclose(fichier);
+        fclose(messagesFp);
+        printf("Message envoyé avec succès aux apprenants du référentiel %s.\n", referentiel);
+        envoyerDesMessages();
+    } else {
+        printf("Erreur lors de l'ouverture des fichiers.\n");
     }
-    return e;
+}
+void envoyerMessageAuxSelectionnes() {
+    FILE *fichier = fopen("utilisateurs.txt", "r");
+    FILE *messagesFp = fopen("messages.txt", "a");
+
+    if (fichier != NULL && messagesFp != NULL) {
+        Utilisateur u;
+
+        printf("\nENVOYER UN MESSAGE AUX APPRENANTS SÉLECTIONNÉS:\n");
+
+        printf("Saisissez les matricules des apprenants à sélectionner (T pour terminer) :\n");
+
+        fprintf(messagesFp, "Destinataire\tMessage\n");
+
+        u.matricule[20];
+        scanf("%s", u.matricule);
+
+        while (strcmp(u.matricule, "T") != 0) {
+            int apprenantExiste = verifierMatricule(u.matricule);
+
+            if (apprenantExiste == 1) {
+                char message[500];
+                printf("Saisissez votre message pour l'apprenant %s (appuyez sur Entrée pour terminer):\n", u.matricule);
+                getchar();
+                fgets(message, sizeof(message), stdin);
+                fprintf(messagesFp, "%s\t%s %s\t%s\n", u.matricule, u.nom, u.prenom, message);
+            } else {
+                printf("Matricule invalide. Veuillez réessayer.\n");
+            }
+
+            printf("Saisissez les matricules des apprenants à sélectionner (T pour terminer) :\n");
+            scanf("%s", matricule);
+        }
+
+        fclose(fichier);
+        fclose(messagesFp);
+        printf("Messages envoyés avec succès aux apprenants sélectionnés.\n");
+        envoyerDesMessages();
+    } else {
+        printf("Erreur lors de l'ouverture des fichiers.\n");
+    }
 }
 void envoyerDesMessages() {
     printf("---------------------------------------------------------------------------------------\n");
     printf("|----------------------Vous avez choisi d'envoyer des messages.-----------------------|\n");
     printf("---------------------------------------------------------------------------------------\n");
-    int nombreMessages;
-    printf("Combien de messages voulez-vous envoyer ? : ");
-    scanf("%d", &nombreMessages);
-    for (int i = 0; i < nombreMessages; i++) {
-        char expediteur[50];
-        char contenu[1000];
+    printf("1………..ENVOYÉ MESSAGE POUR TOUT LES ÉTUDIANTS \n");
+    printf("2………..ENVOYE UN MESSAGE A UN CLASSE \n");
+    printf("3………..ENVOYÉ MESSAGE A UN DES ÉTUDIANTS SÉLECTIONNÉ \n");
+    printf("4………..QUITTER \n");
+    printf(" VEUILLEZ FAIRE UN CHOIX (1 - 4) : ");
 
-        printf("Saisissez l'expéditeur du message %d : ", i + 1);
-        scanf("%s", expediteur);
+    do {
+        scanf("%d", &choix);
 
-        printf("Saisissez le contenu du message %d : ", i + 1);
-        scanf(" %[^\n]s", contenu);
+        switch (choix) {
+            case 1:
+               envoyerMessageATous();
+                break;
+            case 2:
+               envoyerMessageParReferentiel();
+                break;
+            case 3:
+              envoyerMessageAuxSelectionnes();
+                break;
+            case 4:
+                printf("vous avez quitter, à bientôt");
+                printf("vous êtes redirigés dans le menu Admin");
+                break;
+            default:
+                printf("Choix invalide. Veuillez saisir un nombre entre 1 et 5.\n");
+        }
 
-        envoyerMessage(expediteur, contenu);
-    }
+    } while (choix != 4);
 }
 void menuAdmin() {
     printf("---------------------------------------------------------------------------------------\n");
@@ -350,7 +507,7 @@ void menuAdmin() {
                 gestionEtudiants();
                 break;
             case 2:
-                generationfichierUtilisateurs();
+                generationfichiers();
                 break;
             case 3:
                marquerPresences();
@@ -445,11 +602,8 @@ void connexion(){
 }
 
 int main(){
-
     printf("---------------------------------------------------------------------------------------\n");
-    printf("|---------------------------------BIENVENUE DANS L'APPLICATION------------------------|\n");
+    printf("|-------------------BIENVENUE DANS L'APPLICATION SONATEL ACADEMY----------------------|\n");
     printf("---------------------------------------------------------------------------------------\n");
     connexion();
-    connexion();
-
 }
